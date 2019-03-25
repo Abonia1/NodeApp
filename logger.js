fb29c5099c2bc428e1880704b0b3fde96ba278fd
Abonia Sojasingarayar <aboniaa@gmail.com>
2019-03-25 13:18:34 +0100

    various module exports way

--- logger.js
+++ logger.js
@@ -1,3 +1,6 @@
+
+    console.log(__filename);
+    console.log(__dirname);
 //log messages in cloud
 var url='http://mylogger.io/log';
 function log(message){
@@ -11,5 +14,8 @@ function log(message){
 //above line canbe  written also because we do have only one function 
 module.exports=log;
 //module.exports.endPoint=url;
+
+/*exports the logs follow too
+exports.log=log;*/
 
 /* jshint app.js used to find errors in compile time*/
